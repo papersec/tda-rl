2981e7418865daa2fb78f17757fb320310074509
papersec <louis315@naver.com>
2020-06-18 13:35:50 +0900

    gym-test 삭제
    Dqn.py 주석 작성

--- Dqn.py
+++ Dqn.py
@@ -1,3 +1,20 @@
 class Dqn(RLModel):
+
+    # Environment
+
     def __init__(self):
+        # 1. Replay Memory 초기화
+        # 2. Aciton-Value Function Q 초기화
+        # 3. Q hat 초기화
+        # 4. exploration strategy 초기화
+
         pass
+
+    # def learn_episode
+
+    
+    # 1. exploration_strategy에 따라 action 선택
+    # 2. action 실행, reward와 image 받아옴
+    # 3. Replay Memory에 (s_t, a_t, r_t, s_t+1) 저장
+    # 4. Replay Memory에서 minibatch 추출, Gradient Descent
+    # 5. Q hat = Q (every _ step)
--- gym-test/gym-test.py
+++
@@ -1,2 +0,0 @@
-import gym
-env = g
