4958cdb448af93e02810c4b84b8fb71f9096a869
Eunsu Hur <imn8128@gmail.com>
2020-06-18 15:34:46 +0900

    i cloned the huge repository

--- RLModel.py
+++ RLModel.py
@@ -1,3 +1,16 @@
+import gym
+
+
 class RLModel:
+
     def __init__(self):
-        pass
+        self.env = gym.make('CartPole-v0').unwrapped
+        self.time = 0
+
+    def reset_game(self):
+        self.env.reset()
+        self.time = 0
+
+    def conduct_action(self, action):
+        reward, obs, done = self.env.step(action)
+        return reward, obs, done
