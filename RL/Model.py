06e0276c9c95f9aea00617de8e9674aea324504c
papersec <louis315@naver.com>
2020-08-03 16:08:58 +0900

    200803 v2

--- RL/Model.py
+++ RL/Model.py
@@ -7,26 +7,37 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class QNetwork(nn.Module):
-    """Actor (Policy) Model."""
 
     def __init__(self, state_size, action_size):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
-        """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
 
-    def forward(self, state):
-        """Build a network that maps state -> action values."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=4, out_channels=16, kernel_size=8, stride=4, bias=False),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(num_features = 16)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, bias=False),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(num_features = 32)
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Linear(in_features=32 * 9 * 9, out_features=256, bias=False),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(num_features = 256)
+        )
+
+        self.layer4 = nn.Sequential(
+            nn.Linear(in_features=256, out_channels=18, bias=True)
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = x.view(x.shape[0], -1)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x
